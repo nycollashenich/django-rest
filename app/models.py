ca79ccf4d046879c844e147cbb776641bcdc0d26
Nycollas Henich <nhhenich@gmail.com>
2024-08-07 11:02:50 -0300

    ipython

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Tod(models.Model):
+    name = models.CharField(max_length=100)
+    done = models.BooleanField(default=False)
+    created_at = models.DateField(auto_now_add=True)
+
